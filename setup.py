bdca9280355109d9046faf6b50869fe170348b64
Travis F. Collins <travis.collins@analog.com>
2022-05-20 16:39:15 -0600

    Update setup and requirements for new IP features
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='vger-doc-tools',
-    version='0.1.0',
+    name="vger-doc-tools",
+    version="0.1.0",
     packages=find_packages(),
-    package_data={ "vger": ["_templates/*","db/*"] },
+    package_data={"vger": ["_templates/*", "db/*"]},
     include_package_data=True,
     install_requires=[
-        'Click',
+        "Click",
         "jinja2",
+        "symbolator@https://github.com/tfcollins/symbolator/tarball/main",
+        "ipyxact",
     ],
     entry_points={
-        'console_scripts': [
-            'vger = vger.cli:cli',
+        "console_scripts": [
+            "vger = vger.cli:cli",
         ],
     },
 )
